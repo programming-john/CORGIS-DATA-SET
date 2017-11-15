8f7548f55cdfe966c269d17791b6317c95e22fe3
franny1615 <31251485+franny1615@users.noreply.github.com>
2017-11-15 10:50:40 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ def get_country_facts(country):
     fact = ""
     for c in data:
         if c["Country"] == country:
-           fact += Markup("<p>"+ "Immigration from "+ c["Country"]+": "+str(c["Data"]["Legal permanant residences"]["Last Residence"])+"<br>" + "Legal Residents by Birth: " + str(c["Data"]["Legal permanant residences"]["Birth"])+"<br>"+"Year: "+ c["Year"] +"</p>")
+           fact += Markup("<p>"+ "Immigration from "+ c["Country"]+": "+str(c["Data"]["Legal permanant residences"]["Last Residence"])+"<br>" + "Legal Residents by Birth: " + str(c["Data"]["Legal permanant residences"]["Birth"])+"<br>"+"Year: "+ str(c["Year"]) +"</p>")
     return fact
         
 @app.route("/")
