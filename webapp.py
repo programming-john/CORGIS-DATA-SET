859311300cc43cec2e5a7a6734514d7c2af9c424
francisco <franciscofunes5@gmail.com>
2017-11-30 09:40:34 -0800

    added a interactive pie chart

--- webapp.py
+++ webapp.py
@@ -7,6 +7,15 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 with open('immigration.json') as immigrants_data:
         data = json.load(immigrants_data)
 
+def get_years():
+    options = ""
+    firstYear = 2004
+    for c in data:
+        if c["Year"] != firstYear:
+            options += Markup("<option value=" +'"'+ str(c["Year"]) +'"' +">" + str(c["Year"]) + "</option>")
+            firstYear = c["Year"]
+    return options
+
 def get_countries():
     cc = data[0]["Country"]
     countryset = []
@@ -56,8 +65,29 @@ def get_immigration_totals(year):
     total = 0
     for c in data:
         if c["Year"] == year:
-            total += c["Data"]["Naturalizations (Birth)"]
+            if c["Data"]["Naturalizations (Birth)"] != -1:
+                total += c["Data"]["Naturalizations (Birth)"]
     return total
+    
+def get_bilrstat(type, country, year):
+    stat = 0
+    if type == 1:
+        for c in data:
+            if c["Country"] == country:
+                if c["Year"] == int(year):
+                     bi = c["Data"]["Nonimmigrant Admissions"]["Birth"]
+                     lr = c["Data"]["Nonimmigrant Admissions"]["Last Residence"]
+                     whole = bi + lr
+                     stat = (bi/whole)*100
+    if type == 2:
+        for c in data:
+            if c["Country"] == country:
+                if c["Year"] == int(year):
+                     bi = c["Data"]["Nonimmigrant Admissions"]["Birth"]
+                     lr = c["Data"]["Nonimmigrant Admissions"]["Last Residence"]
+                     whole = bi + lr
+                     stat = (lr/whole)*100               
+    return stat
         
 @app.route("/")
 def render_main():
@@ -79,6 +109,10 @@ def render_nat():
 def render_bargraph():
     return render_template('bar.html',fiv = get_immigration_totals(2005),six = get_immigration_totals(2006),sev = get_immigration_totals(2007),eig = get_immigration_totals(2008),nin = get_immigration_totals(2009),ten = get_immigration_totals(2010),ele = get_immigration_totals(2011),twe = get_immigration_totals(2012),thi = get_immigration_totals(2013),fou = get_immigration_totals(2014))
 
+@app.route("/pie")
+def render_pie():
+        return render_template('piechart.html',country=get_countries(), anos=get_years())
+
 @app.route("/act", methods=['GET','POST'])
 def render_result():
     place = request.args['dat']
@@ -93,6 +127,12 @@ def render_resultenforce():
 def render_resultnat():
     place = request.args['d']
     return render_template('natbbirth.html', country=get_countries(), info=get_country_highest(place))
+    
+@app.route("/piechart", methods=['GET','POST'])
+def render_piechart():
+    place = request.args['daf']
+    years  = request.args['leyears']
+    return render_template('piechart.html', country=get_countries(), anos = get_years(), bi = get_bilrstat(1,place, years), lr = get_bilrstat(2,place, years), year = years, palace = place)
 
 
 if __name__=="__main__":
